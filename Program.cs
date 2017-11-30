058e76d1ead2e7f4034f1a65771d9e5d39c74458
Joey Biotti <joey@MacBook-Pro-2.local>
2017-11-29 21:36:38 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace linked_lists
+{
+    class Program
+    {
+        static void Main()
+        {
+            LinkedList <string> linked = new LinkedList<string>();
+
+            linked.AddLast("Cat");
+            linked.AddLast("Dog");
+            linked.AddLast("Man");
+            linked.AddFirst("First");
+
+            foreach (var item in linked)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
